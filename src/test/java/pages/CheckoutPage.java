dd0aa3f6275462f7075f298b621faf5e2f5e67b1
nikita <dunyashenkonikita@mail.ru>
2023-01-09 22:25:59 +0300

    13 successful tests overall

---
+++ src/test/java/pages/CheckoutPage.java
@@ -0,0 +1,52 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CheckoutPage extends BasePage{
+
+    private static final By FIRST_NAME_INPUT = By.id("first-name");
+    private static final By LAST_NAME_INPUT = By.id("last-name");
+    private static final By POSTAL_CODE_INPUT = By.id("postal-code");
+    private static final By CANCEL_BUTTON  = By.id("cancel");
+    private static final By CONTINUE_BUTTON = By.id("continue");
+    private static final By PAGE_NAME = By.cssSelector(".title");
+    private static final By ERROR_MESSAGE = By.cssSelector("[data-test='error']");
+
+    public CheckoutPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public boolean isPageNameDisplays() {
+        if (driver.findElement(PAGE_NAME).isDisplayed()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void fillingCheckoutForm() {
+        driver.findElement(FIRST_NAME_INPUT).sendKeys("Nikita");
+        driver.findElement(LAST_NAME_INPUT).sendKeys("Dunyashenko");
+        driver.findElement(POSTAL_CODE_INPUT).sendKeys("220019");
+    }
+
+    public void fillingCheckoutFormForNegative(String firstName, String lastName, String postalCode) {
+        driver.findElement(FIRST_NAME_INPUT).sendKeys(firstName);
+        driver.findElement(LAST_NAME_INPUT).sendKeys(lastName);
+        driver.findElement(POSTAL_CODE_INPUT).sendKeys(postalCode);
+    }
+
+    public void clickCancelButton() {
+        driver.findElement(CANCEL_BUTTON).click();
+    }
+
+    public void clickContinueButton() {
+        driver.findElement(CONTINUE_BUTTON).click();
+    }
+
+    public String getErrorMessage() {
+        return driver.findElement(ERROR_MESSAGE).getText();
+    }
+
+}
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -4,11 +4,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.testng.ITestContext;
 import org.testng.annotations.*;
-import pages.CartPage;
-import pages.LoginPage;
-import pages.ProductDetailsPage;
-import pages.ProductsPage;
+import pages.*;
 
 import java.util.concurrent.TimeUnit;
 
@@ -20,10 +18,12 @@ public abstract class BaseTest {
     protected ProductsPage productsPage;
     protected CartPage cartPage;
     protected ProductDetailsPage productDetailsPage;
+    protected CheckoutPage checkoutPage;
+    protected CheckoutOverviewPage checkoutOverviewPage;
 
     @Parameters({"browser"})
     @BeforeClass(alwaysRun = true, description = "Setting up the driver")
-    public void setUp(String browserName) {
+    public void setUp(String browserName, ITestContext testContext) {
 
         if (browserName.equals("chrome")) {
             WebDriverManager.chromedriver().setup();
@@ -36,10 +36,14 @@ public abstract class BaseTest {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
+        testContext.setAttribute("driver", driver);
+
         loginPage = new LoginPage(driver);
         productsPage = new ProductsPage(driver);
         cartPage = new CartPage(driver);
         productDetailsPage = new ProductDetailsPage(driver);
+        checkoutPage = new CheckoutPage(driver);
+        checkoutOverviewPage = new CheckoutOverviewPage(driver);
     }
 
     @BeforeMethod(alwaysRun = true, description = "Website URL")
